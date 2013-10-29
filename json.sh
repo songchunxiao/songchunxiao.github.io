b400a9af27b0f63578da5ce58b364c763cc51531
Jie Feng <jokerfeng2010@gmail.com>
2013-10-29 15:37:27 -0400

    Create json.sh

---
+++ json.sh
@@ -0,0 +1,4 @@
+wget http://pypi.python.org/packages/source/s/simplejson/simplejson-2.0.9.tar.gz#md5=af5e67a39ca3408563411d357e6d5e47
+tar xzvf simplejson-2.0.9.tar.gz
+cd simplejson-2.0.9
+sudo python setup.py install
