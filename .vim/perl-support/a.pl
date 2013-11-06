2360163d4474834fe1578e7df82bc3a76ca3e3ff
JieFeng <john@ubuntu.(none)>
2013-11-05 19:46:36 -0800

    vim

---
+++ .vim/perl-support/a.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE:  a.pl
+#
+#        USAGE:  ./a.pl  
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Dr. Fritz Mehner (mn), mehner@fh-swf.de
+#      COMPANY:  FH Südwestfalen, Iserlohn
+#      VERSION:  1.0
+#      CREATED:  11/05/2013 06:52:11 PM
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+
+
---
+++ .vim/perl-support/templates/a.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE:  a.pl
+#
+#        USAGE:  ./a.pl  
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Jie Feng (mn), jokerfeng2010@gmail.com
+#      COMPANY:  The Johns Hopkins University
+#      VERSION:  1.0
+#      CREATED:  11/05/2013 07:00:38 PM
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+
+
