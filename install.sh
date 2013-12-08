4ef527e54b4eae7a29651acef68517cf33392e47
Jie Feng <jokerfeng2010@gmail.com>
2013-12-08 17:17:28 -0500

    fengjie

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 ~/install/installVim.sh
 
-#For push.sh
+#push.sh setup
 gpg --output push.sh  --decrypt push.sh.gpg 
 chmod a+x push.sh
 
-#For git setup
+#Git setup
 git config --global core.excludesfile ~/.gitignore_global
 git config --global user.email "jokerfeng2010@gmail.com"
 git config --global user.name "Jie Feng"
 
-#Install softwares
-sudo apt-get install ccrypt vim vim-gnome git expect texlive-full
+#Python setup
+sudo apt-get install python-pip
+sudo pip install urllib2
 
+#Install softwares
+sudo apt-get install vim vim-gnome git expect tomboy 
+sudo apt-get install texlive-full
