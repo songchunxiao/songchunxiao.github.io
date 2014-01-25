cf75e1c19d8695bb387280c8faa3287660e7ce96
Jie Feng <jokerfeng2010@gmail.com>
2014-01-25 00:03:14 -0500

    f

--- .vim/bundle/cvim/c-support/codesnippets/main.cpp
+++ .vim/bundle/cvim/c-support/codesnippets/main.cpp
@@ -28,15 +28,37 @@ using namespace std;
 #define getII(a,b) scanf("%d%d", &a, &b)
 #define getIII(a,b,c) scanf("%d%d%d", &a, &b, &c)
 #define getS(x) scanf("%s", x);
+#define SZ(x) ((int)((x).size()))
+#define REMAX(a,b) (a)=max((a),(b));
+#define DBG(vari) cerr<<#vari<<" = "<<(vari)<<endl;
+#define REMIN(a,b) (a)=min((a),(b));
+#define FOREACH(i,t) for (typeof(t.begin()) i=t.begin(); i!=t.end(); i++)
+#define ALL(t) t.begin(),t.end()
 #define ll long long
 #define ui unsigned int
 #define us unsigned short
+#define IOS ios_base::sync_with_stdio(0);
+#define pb push_back
+#define INF 1001001001
+#define PI 3.1415926535897932384626
+#define mp make_pair
+#define pii pair<int,int>
+#define ll long long
+#define vi vector<int>
+#define fi first
+#define se second
+#define wez(n) int (n); scanf("%d",&(n));
+#define wez2(n,m) int (n),(m); scanf("%d %d",&(n),&(m));
+#define wez3(n,m,k) int (n),(m),(k); scanf("%d %d %d",&(n),&(m),&(k));
+inline void pisz(int n) { printf("%d\n",n); }
+#define TESTS wez(testow)while(testow--)
+#define whileZ int n; getI(n); while(n--)
+#define printA(a,L,R) FE(i,L,R) cout << a[i] << (i==R?'\n':' ')
+template<typename T,typename TT> ostream& operator<<(ostream &s,pair<T,TT> t) {return s<<"("<<t.first<<","<<t.second<<")";}
+template<typename T> ostream& operator<<(ostream &s,vector<T> t){F(i,0,SZ(t))s<<t[i]<<" ";return s; }
 int main ( int argc, char *argv[] ) {
-    //ios_base::sync_with_stdio(0);
-    int T,N,M;
-    getI(T);
-    while(T--){
-
+    int n,m;
+    whileZ{
     }
     return EXIT_SUCCESS;
 }
