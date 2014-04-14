4656b5cd785b75f3be51736725c325bfb485db39
Jie Feng <jokerfeng2010@gmail.com>
2014-04-14 17:42:46 -0400

    f

---
+++ install/nemo.sh
@@ -0,0 +1,41 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: nemo.sh
+# 
+#         USAGE: ./nemo.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 04/14/2014 17:22
+#      REVISION:  ---
+#===============================================================================
+
+if [ -z $1 ]
+then
+    sudo add-apt-repository ppa:noobslab/mint
+    sudo apt-get update
+    sudo apt-get install nemo
+    sudo apt-get install nemo-compare nemo-dropbox nemo-fileroller nemo-pastebin nemo-seahorse nemo-share nemo-rabbitvcs nemo-terminal
+    sudo apt-get install nemo-compare nemo-dropbox nemo-fileroller nemo-pastebin nemo-seahorse nemo-share nemo-rabbitvcs nemo-terminal
+    sudo apt-get install nemo-preview
+elif [ $1 == "rm" ]
+then 
+    sudo apt-get autoremove nemo nemo-*
+    sudo apt-get install ppa-purge
+    sudo ppa-purge ppa:noobslab/mint
+    sudo apt-get update
+elif [ $1 == "conf" ]
+then
+    gsettings set org.nemo.desktop home-icon-visible false; gsettings set org.nemo.desktop trash-icon-visible false; gsettings set org.nemo.desktop computer-icon-visible false; gsettings set org.nemo.desktop volumes-visible false
+
+elif [ $1 == "sound" ]
+then 
+    echo "meow"
+fi
